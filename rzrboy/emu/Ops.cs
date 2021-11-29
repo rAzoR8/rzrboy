4f2f506ae0c1e7fa6f5fce9c8f7b9e9c0c247144
Fabian Wahlster <fwahlster@outlook.com>
2021-11-29 20:01:52 +0200

    jumps

--- rzrboy/emu/Ops.cs
+++ rzrboy/emu/Ops.cs
@@ -10,6 +10,9 @@
             public static dis operand(Reg8 reg) => (pc, mem) => reg.ToString();
             public static dis operand(Reg16 reg) => (pc, mem) => reg.ToString();
 
+            public static dis operandDB8() => (pc, mem) => $"0x{mem[pc]:X1}";
+            public static dis operandDB16() => (pc, mem) => $"0x{mem[pc]:X1}{mem[(ushort)(pc+1)]:X1}";
+
             public static dis[] operand(Reg8 dst, Reg8 src) => new dis[] { operand(dst), operand(src) };
             public static dis[] operand(Reg16 dst, Reg16 src) => new dis[] { operand(dst), operand(src) };
 
@@ -55,6 +58,36 @@
                 yield return (Reg reg, Mem mem) => { mem[nn] = binutil.lsb(reg.SP); return true; };
                 yield return (Reg reg, Mem mem) => { mem[++nn] = binutil.msb(reg.SP); return true; };
             }
+
+            private static op jp(ushort addr) => (reg, mem) => { reg.PC = addr; return true; };
+            // JP HL
+            public static op jphl() => (reg, mem) => { reg.PC = reg.HL; return true; };
+
+            public delegate bool cond(Reg reg);
+            public readonly static cond NZ = (Reg reg) => !reg.Zero;
+            public readonly static cond Z = (Reg reg) => reg.Zero;
+            public readonly static cond NC = (Reg reg) => !reg.Carry;
+            public readonly static cond C = (Reg reg) => reg.Carry;
+
+            // JP cc, a16
+            public static IEnumerable<op> jpccimm16(cond cc)
+            {
+                byte nlow = 0, nhigh = 0;
+                yield return ldimm(nlow);
+                yield return (Reg reg, Mem mem) => { nhigh = mem[reg.PC++]; return cc(reg); }; ;
+                ushort nn = binutil.Combine(nhigh, nlow);                
+                yield return jp(nn);
+            }
+
+            // JP a16
+            public static IEnumerable<op> jpimm16()
+            {
+                byte nlow = 0, nhigh = 0;
+                yield return ldimm(nlow);
+                yield return ldimm(nhigh);
+                ushort nn = binutil.Combine(nhigh, nlow);
+                yield return jp(nn);
+            }
         };
 
         private static Builder ldimm(Reg8 target) => Ops.ldimm(target).Get("LD") + Ops.operand(target) + ".DB8";
@@ -67,5 +100,7 @@
         private static Builder ldadr(Reg16 dst_addr, Reg8 src) => Ops.ldadr(dst_addr, src).Get("LD") + $"({dst_addr})" + Ops.operand(src);
 
         private static Builder ldimm16_sp() => new Builder(Ops.ldimm16_sp(), "LD") + "(.DB16)" + "SP";
+
+        private static Builder jpimm16() => new Builder(Ops.jpimm16(), "JP") + Ops.operandDB16();
     }
 }
