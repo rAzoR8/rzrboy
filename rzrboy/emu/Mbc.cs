2bc887f4589b1a450abbf446a53f8785291b191f
Fabian Wahlster <fwahlster@outlook.com>
2022-01-29 18:28:04 +0200

    separate mbc1

--- rzrboy/emu/Mbc.cs
+++ rzrboy/emu/Mbc.cs
@@ -10,9 +10,9 @@ namespace rzr
         private byte[] m_rom;
 		private byte[] m_ram;
 
-		private int m_selectedRomBank = 0;
-		private int m_selectedRamBank = 0;
-		private bool m_ramEnabled = false;
+		protected int m_selectedRomBank = 0;
+		protected int m_selectedRamBank = 0;
+		protected bool m_ramEnabled = false;
 
         public byte[] Ram() => m_ram;
         public byte[] Rom() => m_rom;
@@ -34,9 +34,10 @@ namespace rzr
 		public override bool Contains( ushort address )
 		{
             return
-                address < 0x8000 || // roms
-                ( address > 0x9FFF && // 0x8000-0x9FFF vram
-                address < 0xC000 ); // eram
+                address < 0x8000 ||     // roms banks
+                                        // 0x8000-0x9FFF vram
+                ( address > 0x9FFF &&   // 0xA000 - 0xC000 eram
+                address < 0xC000 );     
         }
 
         // mapped access for emulator, default impl
@@ -49,7 +50,7 @@ namespace rzr
                     var bankAdr = ( m_selectedRomBank * RomBanks ) + address - StartAddr;
                     return m_rom[bankAdr];
                 }
-                else // ram
+                else // ram, TODO: m_ramEnabled
                 {
                     var bankAdr = ( m_selectedRamBank * RamBanks ) + address - StartAddr;
                     return m_ram[bankAdr];
@@ -62,24 +63,13 @@ namespace rzr
                     var bankAdr = ( m_selectedRomBank * RomBanks ) + address - StartAddr;
                     m_rom[bankAdr] = value;
                 }
-                else // ram
+                else // ram, TODO: m_ramEnabled
                 {
                     var bankAdr = ( m_selectedRamBank * RamBanks ) + address - StartAddr;
                     m_ram[bankAdr] = value;
                 }
             }
         }
-
-        enum BankingMode : int
-		{
-			SimpleRomBanking = 0,
-			RamBanking = 1
-		}
-
-		private BankingMode m_bankingMode = default;
-
-		private int m_primaryRomBank = 0;
-		private int m_secondaryRomBank = 0;
 
         public int RomBanks
         {
@@ -104,30 +94,53 @@ namespace rzr
                 }
             }
         }
+    }
 
-        private void MBC1RegWrite( ushort address, byte value )
+	public class Mbc1 : Mbc
+	{
+        enum BankingMode : int
         {
-            if( address >= 0x0000 && address <= 0x1FFF ) // enable ram
-            {
-                m_ramEnabled = ( value & 0b1111 ) == 0xA;
-                return;
-            }
-            else if( address >= 0x2000 && address <= 0x3FFF ) // rom bank number lower 5 bits
-            {
-                m_primaryRomBank = (byte)( value & 0b11111 );
-            }
-            else if( address >= 0x4000 && address <= 0x5FFF ) // rom bank number upper 2 bits
-            {
-                m_secondaryRomBank = (byte)( value & 0b11 );
-            }
-            else if( address >= 0x6000 && address <= 0x7FFF ) // banking mode select
-            {
-                m_bankingMode = (BankingMode)( ( value & 0b1 ) );
-            }
-
-            m_selectedRomBank = ( ( m_secondaryRomBank << 5 ) + m_primaryRomBank ) % RomBanks;
-            Debug.WriteLine( $"[{address:X4}:{value:X2}] Selected rom{m_selectedRomBank} [{m_primaryRomBank}:{m_secondaryRomBank}]" );
+            SimpleRomBanking = 0,
+            RamBanking = 1
         }
 
+        private BankingMode m_bankingMode = default;
+
+        private int m_primaryRomBank = 0;
+        private int m_secondaryRomBank = 0;
+
+        public Mbc1( byte[] rom ) : base( rom )
+		{
+		}
+
+        public override byte this[ushort address]
+        {
+            set
+            {
+                if( address >= 0x0000 && address <= 0x1FFF ) // enable ram
+                {
+                    m_ramEnabled = ( value & 0b1111 ) == 0xA;
+                }
+                else if( address >= 0x2000 && address <= 0x3FFF ) // rom bank number lower 5 bits
+                {
+                    m_primaryRomBank = (byte)( value & 0b11111 );
+                }
+                else if( address >= 0x4000 && address <= 0x5FFF ) // rom bank number upper 2 bits
+                {
+                    m_secondaryRomBank = (byte)( value & 0b11 );
+                }
+                else if( address >= 0x6000 && address <= 0x7FFF ) // banking mode select
+                {
+                    m_bankingMode = (BankingMode)( ( value & 0b1 ) );
+                }
+                else if( address >= 0xA000 ) // ram
+                {
+                    base[address] = value;
+                }
+
+                m_selectedRomBank = ( ( m_secondaryRomBank << 5 ) + m_primaryRomBank ) % RomBanks;
+                Debug.WriteLine( $"[{address:X4}:{value:X2}] Selected rom{m_selectedRomBank} [{m_primaryRomBank}:{m_secondaryRomBank}]" );
+            }
+        }
     }
 }
