da68dc7d94dcf90b9574f0c52402fa2954b142c8
Fabian Wahlster <fwahlster@outlook.com>
2022-03-13 12:44:01 +0200

    wip picker stuff

--- rzrboy/emu/ExtensionMethods.cs
+++ rzrboy/emu/ExtensionMethods.cs
@@ -7,20 +7,20 @@
 			return vals.Select( v => v.ToString() );
 		}
 
-		public static IEnumerable<T> EnumValues<T>() where T : System.Enum
+		public static T[] EnumValues<T>() where T : struct, System.Enum
 		{
-			return Enum.GetValues( typeof( T ) ).Cast<T>();
+			return Enum.GetValues<T>();
 		}
 
-		public static IEnumerable<T> EnumValues<T>( this T from ) where T : System.Enum
+		public static IEnumerable<T> EnumValues<T>( this T from ) where T : struct, System.Enum
 		{
-			return Enum.GetValues( typeof( T ) ).Cast<T>().Where( v => v.CompareTo( from ) > -1 );
+			return Enum.GetValues<T>().Where( v => v.CompareTo( from ) > -1 );
 		}
 
 		// (from, to) inclusive ragne from-to
-		public static IEnumerable<T> EnumValues<T>( this T from, T to ) where T : System.Enum
+		public static IEnumerable<T> EnumValues<T>( this T from, T to ) where T : struct, System.Enum
 		{
-			return Enum.GetValues( typeof( T ) ).Cast<T>().Where( v => v.CompareTo( from ) > -1 && v.CompareTo( to ) <= 0 );
+			return Enum.GetValues<T>( ).Where( v => v.CompareTo( from ) > -1 && v.CompareTo( to ) <= 0 );
 		}
 	}
 }
--- rzrboy/rzrboy/InstructionPicker.cs
+++ rzrboy/rzrboy/InstructionPicker.cs
@@ -7,32 +7,22 @@ namespace rzrboy
 {
 	public class InstructionPicker : StackLayout
 	{
-		private Button m_Instruction = new() { Text = "Instr" };
+		private Picker m_InstrPicker = new Picker { ItemsSource = new List<rzr.InstrType>(rzr.InstrType.Db.EnumValues()) }; // fixed list picking
 		private Button m_Lhs = new() { Text = "Lhs" };
 		private Button m_Rhs = new() { Text = "Rhs" };
 
 		private HorizontalStackLayout m_Buttons;
 
-		private Picker m_Picker = new(); // fixed list picking
 		private Entry m_Entry = new(); // value / number picking
-
-		private HorizontalStackLayout m_ValueSelector;
-
-		private List<rzr.InstrType> m_InstrTypes = new ( rzr.InstrType.Nop.EnumValues( rzr.InstrType.Jr ) );
 
 		public InstructionPicker()
 		{
-			m_Buttons = new HorizontalStackLayout { m_Instruction, m_Lhs, m_Rhs };
-			m_ValueSelector = new HorizontalStackLayout { m_Picker, m_Entry };
+			m_Buttons = new HorizontalStackLayout { m_InstrPicker, m_Lhs, m_Rhs };
 
 			Children.Add( m_Buttons );
-			Children.Add( m_ValueSelector );
 
-			m_Instruction.Clicked += OnInstrButtonPressed;
 			m_Lhs.Clicked += OnLhsButtonPressed;
 			m_Rhs.Clicked += OnRhsButtonPressed;
-
-			m_Picker.ItemsSource = m_InstrTypes;
 		}
 
 		void OnInstrButtonPressed( object sender, EventArgs args )
