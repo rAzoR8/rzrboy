171b1944f7418f4c24a4c534313111c61ab97a6c
Fabian Wahlster <fwahlster@outlook.com>
2023-03-04 16:57:12 +0100

    add wram file

---
+++ rzrboy/emu/WRam.cs
@@ -0,0 +1,11 @@
+﻿namespace rzr
+{
+	public class WRam : ISection
+	{
+		public byte this[ushort address] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public string Name = "WRAM";
+		public ushort StartAddr => 0xC000;
+		public ushort Length => 8192; // 8KiB
+	}
+}
