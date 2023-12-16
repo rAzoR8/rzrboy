b34589d8dfeecb82c130a6d8f7abc81bfc13f31e
Fabian Wahlster <fwahlster@outlook.com>
2023-12-16 14:07:24 +0200

    fix IO access in register window

--- rzrboy/Debugger/RegisterWindow.cs
+++ rzrboy/Debugger/RegisterWindow.cs
@@ -29,7 +29,7 @@ namespace dbg.ui
 
 			ImGui.Separator();
 
-			for( ushort i = 0xFF00; i < 0xFF00 + 0x7F; )
+			for( ushort i = 0xFF00; i < 0xFF00 + 0x7F-1; )
 			{
 				ImGui.Text( $"{i:X4}: {mem[i++]:X2} | {i:X4}: {mem[i++]:X2}" );
 			}
